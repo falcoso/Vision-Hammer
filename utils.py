d55248da7a0d5ec4d88e7e5430559dd816a8dd95
Oliver Jones <ogj21@cam.ac.uk>
2019-10-28 21:47:10 +0000

    Combined Point clouds   - Created cleaned up versions of each cloud   - Single cleaned up cloud   - Fixed bugs in utils   - added examples that extract/ fit a single plane

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import open3d as o3d
 import numpy as np
 
 
-def hist_normals(pcd, bin_size=0.95, line_set=False):
+def hist_normals(pcd, bin_size=0.95):
     """
     Returns a list of most common normals ordered by magnitude for a given point
     cloud. All vectors returned are normalised by the largest magnitude.
@@ -15,16 +15,14 @@ def hist_normals(pcd, bin_size=0.95, line_set=False):
     bin_size: float (=0.95)
         tolerance of the dot product to accept a match.
 
-    line_set: bool
-        Returns open3d.LineSet if True
-
     Returns
     -------
     hist_vect: np.array/ open3d.LineSet
     """
     # estimate normals
-    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
-        radius=0.1, max_nn=30))
+    if not pcd.has_normals():
+        pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
+            radius=0.1, max_nn=30))
     normals = np.asarray(pcd.normals)
 
     # create empty list to bin into
@@ -32,6 +30,7 @@ def hist_normals(pcd, bin_size=0.95, line_set=False):
     while len(normals) > 1:
         # select first normal and get dot product with all other normals
         normal = normals[0]
+        normals = normals[1:-1]
         dot_prod = np.abs(normals.dot(normal))
         ind = np.array([i for i in range(len(dot_prod)) if dot_prod[i] > bin_size])
 
@@ -44,16 +43,7 @@ def hist_normals(pcd, bin_size=0.95, line_set=False):
     mags = np.max(np.linalg.norm(vec_list, axis=0))
     vec_list /= mags
 
-    if not line_set:
-        return vec_list
-
-    histogram = o3d.geometry.LineSet()
-    histogram.points = o3d.utility.Vector3dVector(np.append(np.array([[0, 0, 0]]),
-                                                            vec_list, axis=0))
-    lines = np.array([[0, i] for i in range(len(vec_list))])
-    histogram.lines = o3d.utility.Vector2iVector(lines)
-
-    return histogram
+    return vec_list
 
 
 def register_clouds(target, source):
@@ -81,6 +71,12 @@ def register_clouds(target, source):
     voxel_radius = [0.04, 0.02, 0.01]
     max_iter = [50, 30, 14]
     current_transformation = np.identity(4)
+    # calculate normals for point to plane registration
+    for cloud in [source, target]:
+        if not cloud.has_normals():
+            cloud.estimate_normals(
+                o3d.geometry.KDTreeSearchParamHybrid(radius=0.04 * 2,
+                                                     max_nn=30))
     for scale in range(3):
         iter = max_iter[scale]
         radius = voxel_radius[scale]
@@ -89,19 +85,14 @@ def register_clouds(target, source):
         source_down = source.voxel_down_sample(radius)
         target_down = target.voxel_down_sample(radius)
 
-        # calculate normals for point to plane registration
-        for cloud in [source_down, target_down]:
-            if not cloud.has_normals():
-                cloud.estimate_normals(
-                    o3d.geometry.KDTreeSearchParamHybrid(radius=radius * 2,
-                                                         max_nn=30))
-
         result_icp = o3d.registration.registration_colored_icp(
             source_down, target_down, radius, current_transformation,
             o3d.registration.ICPConvergenceCriteria(relative_fitness=1e-6,
                                                     relative_rmse=1e-6,
                                                     max_iteration=iter))
-        return result_icp.transformation
+
+        current_transformation = result_icp.transformation
+    return result_icp.transformation
 
 
 def clean_cloud(pcd_fp, std_ratio=1, nb_neighbors=20, view=False, pcd_sl=None):
@@ -134,9 +125,12 @@ def clean_cloud(pcd_fp, std_ratio=1, nb_neighbors=20, view=False, pcd_sl=None):
     else:
         pcd = pcd_fp
 
-    cl, ind = pcd.remove_statistical_outlier(nb_neighbors=20,
-                                             std_ratio=1)
+    cl, ind = pcd.remove_statistical_outlier(nb_neighbors=nb_neighbors,
+                                             std_ratio=std_ratio)
     if view:
+        if not pcd.has_normals():
+            pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
+                radius=0.1, max_nn=30))
         display_inlier_outlier(pcd, ind)
 
     if pcd_sl is not None:
@@ -162,3 +156,36 @@ def display_inlier_outlier(cloud, ind):
     inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
     o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
     return
+
+
+def create_vector_graph(vectors):
+    """Creats a Line Geometry of the supplied vectors coming from the origin.
+
+    Parameters
+    ----------
+    vectors : list of 3d vectors
+        List of vectors to form geometry from
+
+    Returns
+    -------
+    line_set : o3d.geometry.LineSet()
+        Geometry of vectors to be displayed
+
+    """
+    # create points with 0 vector
+    points = np.append(np.zeros((1, 3)), vectors, axis=0)
+
+    # create set of lines each starting from 0,0,0
+    lines = np.array([[0, i+1]for i in range(len(vectors))])
+
+    line_set = o3d.geometry.LineSet()
+    line_set.points = o3d.utility.Vector3dVector(points)
+    line_set.lines = o3d.utility.Vector2iVector(lines)
+    return line_set
+
+
+if __name__ == "__main__":
+    pcd = o3d.io.read_point_cloud("./Point Clouds/combined_cloud_clean.ply")
+    lines = hist_normals(pcd)
+    line_set = create_vector_graph(lines)
+    o3d.visualization.draw_geometries([pcd, line_set])
